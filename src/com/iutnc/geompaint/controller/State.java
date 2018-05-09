bc21a83a679f04a60e8ab50c53b1e56e7c4e1055
fdeblic <fdeblic@hotmail.com>
2018-05-09 21:11:04 +0200

    State enum comment added

--- src/com/iutnc/geompaint/controller/State.java
+++ src/com/iutnc/geompaint/controller/State.java
@@ -1,5 +1,11 @@
 package com.iutnc.geompaint.controller;
 
+/**
+ * Represents the state of the program
+ * may allow actions, disable others...
+ * @author francois
+ *
+ */
 public enum State {
 	NORMAL, DRAWING, DRAGGING
 }
