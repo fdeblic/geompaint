612c4b4e5abaf9fbf3022cabf54111961fa5e718
Elise <moreau.elise13@gmail.com>
2018-05-11 11:30:34 +0200

    add functions

--- src/com/iutnc/geompaint/view/Canvas.java
+++ src/com/iutnc/geompaint/view/Canvas.java
@@ -36,16 +36,27 @@ public class Canvas extends JPanel implements MouseListener, MouseMotionListener
 	 * @param selectedFigure the selected figure, you want to modify
 	 * @param movingPoint the point to move a figure
 	 */
-	public Canvas() {
+	public Canvas(GeomPaint controller) {
 		super();
 	}
+	
+	
+	public void addFigure(Figure f){}
 
 	/**
 	 * Figure selected to set
 	 * @param selectedFigure the selected figure, you want to modify
 	 */
-	private void setSelectedFigure(Figure selectedFigure) {
-		this.selectedFigure = selectedFigure;
+	private boolean isSelected(Figure f) {
+		return false;
+	}
+	
+	/**
+	 * Figure selected to set
+	 * @param selectedFigure the selected figure, you want to modify
+	 */
+	private boolean isSelected(Point p) {
+		return false;
 	}
 	
 	
