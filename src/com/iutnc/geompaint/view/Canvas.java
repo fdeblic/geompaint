5fe092971f882eb3a5cf649e35912338902594ae
Elise <moreau.elise13@gmail.com>
2018-05-11 10:23:16 +0200

    Add observer + private functions

--- src/com/iutnc/geompaint/view/Canvas.java
+++ src/com/iutnc/geompaint/view/Canvas.java
@@ -1,15 +1,21 @@
 package com.iutnc.geompaint.view;
+import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.util.Observable;
+import java.util.Observer;
+
 import javax.swing.JPanel;
+
+import com.iutnc.geompaint.model.*;
 
 /**
  * Class for canvas
  * @author Elise
  * @version 09/05/2018
  */
-public class Canvas extends JPanel implements MouseListener, MouseMotionListener{
+public class Canvas extends JPanel implements MouseListener, MouseMotionListener, Observer {
 	/**
 	 * Attributs
 	 */
@@ -30,7 +36,7 @@ public class Canvas extends JPanel implements MouseListener, MouseMotionListener
 	 * Figure selected to set
 	 * @param selectedFigure the selected figure, you want to modify
 	 */
-	public void setSelectedFigure(Figure selectedFigure) {
+	private void setSelectedFigure(Figure selectedFigure) {
 		this.selectedFigure = selectedFigure;
 	}
 	
@@ -38,32 +44,32 @@ public class Canvas extends JPanel implements MouseListener, MouseMotionListener
 	 * Draw a figure in the JPanel
 	 * @param f the figure to draw
 	 */
-	public void drawFigure(Figure f) {}
+	private void drawFigure(Figure f) {}
 	
 	/**
 	 * Draw a polygon in the JPanel
 	 * @param p the polygon to draw
 	 */
-	public void drawPolygon(Polygon p) {}
+	private void drawPolygon(Polygon p) {}
 	
 	/**
 	 * Draw a circle in the JPanel
 	 * @param c the circle to draw
 	 */
-	public void drawCircle(Circle c) {}
+	private void drawCircle(Circle c) {}
 	
 	/**
 	 * Draw a point in the JPanel
 	 * @param p the point to draw
 	 */
-	public void drawPoint(Point p) {}
+	private void drawPoint(Point p) {}
 	
 	/**
 	 * The figure is hover
 	 * @param f the figure you want to check
 	 * @return True if the figure is hover
 	 */
-	public boolean isHover(Figure f) {
+	private boolean isHover(Figure f) {
 		return false; 
 	}
 
@@ -87,5 +93,11 @@ public class Canvas extends JPanel implements MouseListener, MouseMotionListener
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
