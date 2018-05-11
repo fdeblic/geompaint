b4d0a788640ee8c285ab092476873560732272c2
Loïc Spacher <loic.spacher@gmail.com>
2018-05-11 14:15:52 +0200

    Creation class FigureTest

---
+++ src/com/iutnc/geompaint/test/FigureTest.java
@@ -0,0 +1,83 @@
+/**
+ * 
+ */
+package com.iutnc.geompaint.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.iutnc.geompaint.model.Figure;
+
+/**
+ * @author Loïc Spacher
+ *
+ */
+class FigureTest {
+
+	/**
+	 * Test method for {@link com.iutnc.geompaint.model.Figure#Figure()}.
+	 */
+	@Test
+	void testFigure() {
+		fail("not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.iutnc.geompaint.model.Figure#addGripPoint(java.awt.Point)}.
+	 */
+	@Test
+	void testAddGripPoint() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.iutnc.geompaint.model.Figure#getGripPoints()}.
+	 */
+	@Test
+	void testGetGripPoints() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.iutnc.geompaint.model.Figure#getNbGripPoints()}.
+	 */
+	@Test
+	void testGetNbGripPoints() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.iutnc.geompaint.model.Figure#isComplete()}.
+	 */
+	@Test
+	void testIsComplete() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.iutnc.geompaint.model.Figure#translate(int, int)}.
+	 */
+	@Test
+	void testTranslate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.iutnc.geompaint.model.Figure#getCopie()}.
+	 */
+	@Test
+	void testGetCopie() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.iutnc.geompaint.model.Figure#removeGripPoint(java.awt.Point)}.
+	 */
+	@Test
+	void testRemoveGripPoint() {
+		fail("Not yet implemented");
+	}
+
+}
