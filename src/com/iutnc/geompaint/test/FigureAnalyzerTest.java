5d46ca497cf06eb1a3462b51f1004b4b83f91938
fdeblic <fdeblic@hotmail.com>
2018-05-14 10:09:10 +0200

    FigureAnalyzerTest class created

---
+++ src/com/iutnc/geompaint/test/FigureAnalyzerTest.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package com.iutnc.geompaint.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author deblic3u
+ *
+ */
+public class FigureAnalyzerTest {
+
+	/**
+	 * Test method for {@link com.iutnc.geompaint.controller.FigureAnalyzer#FigureAnalyzer()}.
+	 */
+	@Test
+	public void testFigureAnalyzer() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.iutnc.geompaint.controller.FigureAnalyzer#FigureAnalyzer(int, int, int, int)}.
+	 */
+	@Test
+	public void testFigureAnalyzerIntIntIntInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.iutnc.geompaint.controller.FigureAnalyzer#setRef(int, int)}.
+	 */
+	@Test
+	public void testSetRef() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.iutnc.geompaint.controller.FigureAnalyzer#setNearTolerence(int)}.
+	 */
+	@Test
+	public void testSetNearTolerence() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.iutnc.geompaint.controller.FigureAnalyzer#setHoverTolerence(int)}.
+	 */
+	@Test
+	public void testSetHoverTolerence() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.iutnc.geompaint.controller.FigureAnalyzer#isNearFigure(com.iutnc.geompaint.model.Figure)}.
+	 */
+	@Test
+	public void testIsNearFigure() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.iutnc.geompaint.controller.FigureAnalyzer#isHoverFigure(com.iutnc.geompaint.model.Figure)}.
+	 */
+	@Test
+	public void testIsHoverFigure() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.iutnc.geompaint.controller.FigureAnalyzer#isNearPoint(java.awt.Point)}.
+	 */
+	@Test
+	public void testIsNearPoint() {
+		fail("Not yet implemented");
+	}
+
+}
