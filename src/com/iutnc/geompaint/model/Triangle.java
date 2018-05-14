004106880b4ed9f70542e0aaf10b9ae0aeadd948
Loïc Spacher <loic.spacher@gmail.com>
2018-05-14 11:40:37 +0200

    Creation of the constructor in Triangle.

--- src/com/iutnc/geompaint/model/Triangle.java
+++ src/com/iutnc/geompaint/model/Triangle.java
@@ -8,10 +8,23 @@ package com.iutnc.geompaint.model;
 
 public class Triangle extends Polygon {
 	
+	public Triangle() {
+		super();
+	}
+	
 	@Override
 	public Triangle getCopie() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-    
+	
+	public boolean isFull() {
+		return this.gripPoints.size()==3;
+		
+	}
+	
+	public boolean isValid() {
+		return this.isFull();
+		
+	}
 }
