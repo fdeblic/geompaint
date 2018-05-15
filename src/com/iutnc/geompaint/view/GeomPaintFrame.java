af9574bfa46cdd330cdf9d5ac4eca84c84ac8cf0
Elise <moreau.elise13@gmail.com>
2018-05-15 11:23:31 +0200

    complete change color function

--- src/com/iutnc/geompaint/view/GeomPaintFrame.java
+++ src/com/iutnc/geompaint/view/GeomPaintFrame.java
@@ -1,9 +1,11 @@
 package com.iutnc.geompaint.view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
 
+import javax.swing.JColorChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -71,7 +73,8 @@ public class GeomPaintFrame extends JFrame implements IGeomPaintView {
 	}
 
 	public void changeFigureColor() {
-		// TODO Auto-generated method stub
+		Color newColor = JColorChooser.showDialog(null, "couleur du fond", Color.WHITE);
+		controller.changeFigureColor(canvas.getSelectedFigure(), newColor);
 		
 	}
 
@@ -86,6 +89,7 @@ public class GeomPaintFrame extends JFrame implements IGeomPaintView {
 
 	public void deleteFigure() {
 		controller.deleteFigure(canvas.getSelectedFigure());
+		//canvas.setSelectedFigure(null);
 		
 	}
 
