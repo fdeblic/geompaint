8307ceb851b317b512316530c604ac56a8c7bba1
fdeblic <fdeblic@hotmail.com>
2018-05-16 22:29:40 +0200

    FigureAnalyzer : hover tests adapted when figure not filled

--- src/com/iutnc/geompaint/controller/FigureAnalyzer.java
+++ src/com/iutnc/geompaint/controller/FigureAnalyzer.java
@@ -19,8 +19,7 @@ public class FigureAnalyzer {
 	// ATTRIBUTES
 	//	
 	
-	private int refX = 0;
-	private int refY = 0;
+	private Point ref = new Point(0, 0);
 	private int tolerance = 5;
 
 	
@@ -59,8 +58,8 @@ public class FigureAnalyzer {
 	 * @param refY Y value used as reference for your tests
 	 */
 	public void setRef(int refX, int refY) {
-		this.refX = refX;
-		this.refY = refY;
+		ref.x = refX;
+		ref.y = refY;
 	}
 	
 	
@@ -105,10 +104,10 @@ public class FigureAnalyzer {
 	 * @return true if the reference is hover the Point (tolerance included), false otherwise
 	 */
 	public boolean isHoverPoint(Point p) {
-		return refX + tolerance >= p.x
-			&& refX - tolerance <= p.x
-			&& refY + tolerance >= p.y
-			&& refY - tolerance <= p.y;
+		return ref.x + tolerance >= p.x
+			&& ref.x - tolerance <= p.x
+			&& ref.y + tolerance >= p.y
+			&& ref.y - tolerance <= p.y;
 	}
 	
 	//
@@ -120,11 +119,11 @@ public class FigureAnalyzer {
 	}
 	
 	public int getRefX() {
-		return refX;
+		return ref.x;
 	}
 	
 	public int getRefY() {
-		return refY;
+		return ref.y;
 	}
 	
 	//
@@ -134,34 +133,85 @@ public class FigureAnalyzer {
 	/**
 	 * Checks if the reference coordinates are on the Circle given in parameter with the tolerance fixed before
 	 * @param c the Circle
+	 * @param tolerance the tolerance in pixels
 	 * @return true if the reference is on the Circle (tolerance included), false otherwise
 	 */
 	private boolean isOnCircle(Circle c, int tolerance) {
 		int x, y;
 		Point centre = c.getCentre();
 
-		x = refX - centre.x;
-		y = refY - centre.y;
+		x = ref.x - centre.x;
+		y = ref.y - centre.y;
 
 		// Calculates the complex module : |z| = sqrt(a*a + b*b)
-		return c.getRadius() + tolerance >= Math.sqrt(x*x + y*y);
+		int z = (int) Math.sqrt(x*x + y*y);
+
+		if (c.isFilled())
+			return c.getRadius() + tolerance >= z;
+		else
+			return c.getRadius() + tolerance >= z && c.getRadius() - tolerance <= z;
 	}
 	
 	/**
-	 * Checks if the reference coordinates are on the Polygon given in parameter with the tolerance fixed before
+	 * Checks if the reference coordinates are on the Polygon given in parameter
 	 * @param p the Polygon
+	 * @param tolerance the tolerence in pixels
 	 * @return true if the reference is on the Polygon (tolerance included), false otherwise
 	 */
 	private boolean isOnPolygon(Polygon p, int tolerance) {
-		java.awt.Polygon polygon = new java.awt.Polygon();
-		for (Point pt : p.getPoints())
-			polygon.addPoint(pt.x, pt.y);
-		for (int x = refX - tolerance ; x <= refX + tolerance ; x++) {
-			for (int y = refY - tolerance ; y <= refY + tolerance ; y++) {
-				if (polygon.contains(new Point(x, y)))
-					return true;
+		if (p.isFilled()) {
+			java.awt.Polygon polygon = new java.awt.Polygon();
+			for (Point pt : p.getPoints())
+				polygon.addPoint(pt.x, pt.y);
+			for (int x = ref.x - tolerance ; x <= ref.x + tolerance ; x++) {
+				for (int y = ref.y - tolerance ; y <= ref.y + tolerance ; y++) {
+					if (polygon.contains(new Point(x, y)))
+						return true;
+				}
 			}
+			return false;
+		} else {
+			Point[] pts = p.getPoints();
+			boolean res = false;
+			for (int i = 0 ; i < pts.length ; i++) {
+				if (i == 0) {
+					if (isOnLine(pts[i] , pts[pts.length-1], tolerance)) {
+						res = true;
+					}
+				} else {
+					if (isOnLine(pts[i-1] , pts[i], tolerance)) {
+						res = true;
+					}
+				}
+			}
+			return res;
 		}
-		return false;
+	}
+	
+	/**
+	 * Checks if the reference coordinates are on the segment between p1 and p2
+	 * @param p1 first point of the segment
+	 * @param p2 second point of the segment
+	 * @param tolerance the tolerance in pixels
+	 * @return true if the reference is on the line (tolerance included), false otherwise
+	 */
+	private boolean isOnLine(Point p1, Point p2, int tolerance) {
+		if (ref.x + tolerance <= p1.x && ref.x + tolerance <= p2.x) return false;
+		if (ref.x - tolerance >= p1.x && ref.x - tolerance >= p2.x) return false;
+		if (ref.y + tolerance <= p1.y && ref.y + tolerance <= p2.y) return false;
+		if (ref.y - tolerance >= p1.y && ref.y - tolerance >= p2.y) return false;
+		
+		// https://stackoverflow.com/questions/30559799/function-for-finding-the-distance-between-a-point-and-an-edge-in-java
+		int A = ref.x - p1.x; // position of point rel one end of line
+		int B = ref.y - p1.y;
+		int C = p2.x - p1.x; // vector along line
+		int D = p2.y - p1.y;
+		int E = -D; // orthogonal vector
+		int F = C;
+		
+		float dot = A * E + B * F;
+		float len_sq = E * E + F * F;
+		
+		return (float) (Math.abs(dot) / Math.sqrt(len_sq)) <= tolerance;
 	}
 }
