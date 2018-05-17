85835b0a993351b6e7236f8c75080747570a8d3d
Elise <moreau.elise13@gmail.com>
2018-05-17 10:54:01 +0200

    complete moveUp and moveDown functions

--- src/com/iutnc/geompaint/view/GeomPaintFrame.java
+++ src/com/iutnc/geompaint/view/GeomPaintFrame.java
@@ -72,13 +72,19 @@ public class GeomPaintFrame extends JFrame implements IGeomPaintView {
 	public void enableAdding(boolean b) {
 		menuAdd.setEnabled(b);
 	}
-
+	
+	/**
+	 * Change the color of the selected figure
+	 */
 	public void changeFigureColor() {
 		if (canvas.getSelectedFigure() == null) return;
 		Color newColor = JColorChooser.showDialog(null, "Couleur du fond", canvas.getSelectedFigure().getColor());
 		controller.changeFigureColor(canvas.getSelectedFigure(), newColor);
 	}
 
+	/**
+	 * Fill and clean the selected figure 
+	 */
 	public void fillFigure() {
 		if (canvas.getSelectedFigure() == null) return;
 		
@@ -90,24 +96,36 @@ public class GeomPaintFrame extends JFrame implements IGeomPaintView {
 		}
 	}
 
+	/**
+	 * Clone the selected figure
+	 */
 	public void cloneFigure() {
 		Figure newFigure = controller.cloneFigure(canvas.getSelectedFigure());
 		canvas.setSelectedFigure(newFigure);
 		
 	}
 
+	/**
+	 * Delete the selected figure
+	 */
 	public void deleteFigure() {
 		controller.deleteFigure(canvas.getSelectedFigure());
 		canvas.setSelectedFigure(null);
 		
 	}
 
+	/**
+	 * Create a circle
+	 */
 	public void createCircle() {
 		canvas.cancelDrawing();
 		canvas.setSelectedFigure(new Circle());
 		canvas.setState(State.DRAWING);
 	}
 
+	/**
+	 * Create a polygon
+	 */
 	public void createPolygon() {
 		menuAdd.setEnabled(false);
 		menuEdit.setEnabled(false);
@@ -115,45 +133,78 @@ public class GeomPaintFrame extends JFrame implements IGeomPaintView {
 		canvas.setState(State.DRAWING);
 	}
 
+	/**
+	 * Create a rectangle
+	 */
 	public void createRectangle() {
 		canvas.setSelectedFigure(new Rectangle());
 		canvas.setState(State.DRAWING);
 	}
 
+	/**
+	 * Create a triangle
+	 */
 	public void createTriangle() {
 		canvas.setSelectedFigure(new Triangle());
 		canvas.setState(State.DRAWING);
 	}
 
+	/**
+	 * Get the figures's list
+	 * @return the figures's list
+	 */
 	public Figure[] getFigures() {
 		return controller.getFigures();
 	}
 
+	/**
+	 * Save a figure in the figures's list
+	 * @param f the figure to save
+	 */
 	public void saveFigure(Figure f) {
 		controller.addFigure(f);
 		canvas.setState(State.NORMAL);
 	}
 
+	/**
+	 * Move a point 
+	 * @param p the point to move
+	 * @param x the abscissa 
+	 * @param y the ordinate
+	 */
 	public void movePoint(Point p, int x, int y) {
 		controller.movePoint(p, x, y);
 	}
+	
+	/**
+	 * Move a figure 
+	 * @param f the figure to move
+	 * @param x the abscissa 
+	 * @param y the ordinate
+	 */
 	
 	public void moveFigure(Figure f, int x, int y){
 		controller.moveFigure(f, x, y);
 	}
 	
+
 	public Canvas getObserver() {
 		return canvas;
 	}
 
+	/**
+	 * Move the selected figure on the front
+	 */
 	public void moveUp() {
 		controller.moveUp(canvas.getSelectedFigure());
 		
 	}
 
+	/**
+	 * Move the selected figure on the back
+	 */
 	public void moveDown() {
-		// TODO Auto-generated method stub
-		
+		controller.moveDown(canvas.getSelectedFigure());		
 	}
 
 }
