60a845490a9509e2a9d1ac366c07c861ec49b4d2
fdeblic <fdeblic@hotmail.com>
2018-05-17 19:34:44 +0200

    User warned if icons or font are not found

--- src/com/iutnc/geompaint/Principale.java
+++ src/com/iutnc/geompaint/Principale.java
@@ -1,5 +1,11 @@
 package com.iutnc.geompaint;
 
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JOptionPane;
 import com.iutnc.geompaint.controller.GeomPaintController;
 import com.iutnc.geompaint.view.*;
 
@@ -13,6 +19,16 @@ public class Principale {
 	public static void main(String[] args) {
 		GeomPaintController controller = new GeomPaintController();
 		GeomPaintFrame fenetre = new GeomPaintFrame(controller);
+		
+		if (!new File("img/icons.png").exists())
+			JOptionPane.showMessageDialog(fenetre, "Impossible de trouver l'image img/icons.png", "Icônes manquantes", JOptionPane.WARNING_MESSAGE);
+
+		try {
+			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("img/font/calibrib.ttf")));
+		} catch (IOException|FontFormatException e) {
+			JOptionPane.showMessageDialog(fenetre, "Impossible de trouver la police img/font/calibrib.ttf", "Police manquante", JOptionPane.WARNING_MESSAGE);
+		}
 		controller.setView(fenetre);
 		fenetre.setVisible(true);
 	}
